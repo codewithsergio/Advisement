d0249469e7e5c05ada5f9b5d2eefe3d6898f015e
Sergio Hernandez <ehernandez.sergio@gmail.com>
2023-04-05 22:20:23 -0700

    Login Page redesign, Student Records Redesign

--- src/Login.js
+++ src/Login.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import './Login.css';
-import { signInWithGoogle } from './firebase-config';
+import React from "react";
+import "./Login.css";
+import { signInWithGoogle } from "./firebase-config";
 
-function Login({setter}) {
-    const signUsIn = () => {
-        signInWithGoogle(setter); 
-    }
+function Login({ setter }) {
+  const signUsIn = () => {
+    signInWithGoogle(setter);
+  };
   return (
     <div className="loginpage">
-        <div className="login_page_title">CADV Advisement Admin Login</div>
-        <button onClick={signUsIn}>Log In</button>
+      <div className="loginCard">
+        <img src="./csun_logo.png" alt="csun logo" />
+        <h1>CADV Advisement Admin Portal</h1>
+        <p>Helping Advisors Help Students Succeed</p>
+        <button onClick={signUsIn}>LOGIN</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- src/StudentRecords.js
+++ src/StudentRecords.js
@@ -1,82 +1,114 @@
-import React, { useEffect, useState } from 'react'
-import './StudentRecords.css'
-import { updateDoc, doc, onSnapshot } from "firebase/firestore"; 
-import {db} from './firebase-config';
-import {CSVLink} from 'react-csv';
-import { Icon } from "@iconify/react";
+import React, { useEffect, useState } from "react";
+import "./StudentRecords.css";
+import { updateDoc, doc, onSnapshot } from "firebase/firestore";
+import { db } from "./firebase-config";
+import { CSVLink } from "react-csv";
 
-function StudentRecords({user}) {
-    const [theme, setTheme] = useState('Light');
-    const toggleTheme = () => {
-        if(theme === 'Light'){
-            setTheme('Dark');
-        } else {
-            setTheme('Light');
-        }
-    };
-    const [recordsList, setRecordsList] = useState([]);
-    const todaysDate = new Date();
+function StudentRecords({ user }) {
+  const [theme, setTheme] = useState("Light");
+  const toggleTheme = () => {
+    if (theme === "Light") {
+      setTheme("Dark");
+    } else {
+      setTheme("Light");
+    }
+  };
+  const [recordsList, setRecordsList] = useState([]);
+  const todaysDate = new Date();
 
-    const confirmRecordDeletion = () => {
-        if(window.confirm('This will permanently remove all student records for all users and cannot be undone. Are you sure you want to proceed with deletion?') === true){
-            // Delete records
-            updateDoc(doc(db, "records", "recordArray"), {
-                information: []
-              })
-              .then(() => {
-                alert(`All data has been deleted!`);
-              })
-              .catch((error) => {
-                alert(error.message);
-            });
-        }
-    };
-
-    useEffect(() => {
-        // When student submits form, re render page.
-        onSnapshot(doc(db, "records", "recordArray"), (doc) => {
-            setRecordsList(doc.data().information.reverse());
+  const confirmRecordDeletion = () => {
+    if (
+      window.confirm(
+        "This will permanently remove all student records for all users and cannot be undone. Are you sure you want to proceed with deletion?"
+      ) === true
+    ) {
+      // Delete records
+      updateDoc(doc(db, "records", "recordArray"), {
+        information: [],
+      })
+        .then(() => {
+          alert(`All data has been deleted!`);
+        })
+        .catch((error) => {
+          alert(error.message);
         });
-    }, []);
+    }
+  };
+
+  useEffect(() => {
+    // When student submits form, re render page.
+    onSnapshot(doc(db, "records", "recordArray"), (doc) => {
+      setRecordsList(doc.data().information.reverse());
+    });
+  }, []);
 
   return (
     <div className={`StudentRecords ${theme}`}>
-        <div className="allowedView">
-            <h1>Student Records</h1>
-            <p>Hello {user[0]}!</p>
+      <div className="allowedView">
+        <div className="navbar">
+          <h1>Student Records</h1>
+          <div className="tabOptions">
             <CSVLink
-            className="CSVButton"
-            filename={`StudentRecords_${todaysDate.toLocaleString()}.csv`}
-            data={[['Date', 'Name', 'Student ID', 'Reason'], ...recordsList.map(record => [record.date, record.name, record.studentId, record.reason])]}
-            >Download CSV File</CSVLink>
-            <button className="deleteRecordsButton"
-            onClick={() => {confirmRecordDeletion()}}>DELETE Records</button>
-            <div className="themeButtonDiv">
-                <button className="themeButton" onClick={() => {toggleTheme()}}>
-                    <Icon className="darkModeIcon" icon={"gg:dark-mode"}/>
-                </button>
-            </div>
-            <table>
-                <tbody>
-                    <tr>
-                        <th>Date</th>
-                        <th>Name</th>
-                        <th>Student ID</th>
-                        <th>Reason</th>
-                    </tr>
-                    {recordsList.map((record, index) => (
-                        <tr key={index}>
-                            <td>{record.date}</td>
-                            <td>{record.name}</td>
-                            <td>{record.studentId}</td>
-                            <td>{record.reason}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+              className="CSVButton"
+              filename={`StudentRecords_${todaysDate.toLocaleString()}.csv`}
+              data={[
+                ["Date", "Name", "Student ID", "Reason"],
+                ...recordsList.map((record) => [
+                  record.date,
+                  record.name,
+                  record.studentId,
+                  record.reason,
+                ]),
+              ]}
+            >
+              Download CSV File
+            </CSVLink>
+            <button
+              className="deleteRecordsButton"
+              onClick={() => {
+                confirmRecordDeletion();
+              }}
+            >
+              DELETE Records
+            </button>
+          </div>
         </div>
+        <div className="userMisc">
+          <p>
+            <span>Current User:</span> {user[0]}
+          </p>
+          <div
+            className="slider"
+            onClick={() => {
+              toggleTheme();
+            }}
+          >
+            <label>🌙</label>
+            <div className="ball"></div>
+            <label>☀️</label>
+          </div>
+        </div>
+        <table>
+          <tbody>
+            <tr>
+              <th>Date</th>
+              <th>Name</th>
+              <th>Student ID</th>
+              <th>Reason</th>
+            </tr>
+            {recordsList.map((record, index) => (
+              <tr key={index}>
+                <td>{record.date}</td>
+                <td>{record.name}</td>
+                <td>{record.studentId}</td>
+                <td>{record.reason}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
+  );
 }
 
-export default StudentRecords
+export default StudentRecords;
